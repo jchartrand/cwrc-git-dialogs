333b2d02e683f3f9df8c96c9bbaa418359c7c539
Andrew <ajmacdonald@users.noreply.github.com>
2019-09-19 12:41:52 -0400

    fix: add list keys, use className instead of class

--- src/SearchResultList.js
+++ src/SearchResultList.js
@@ -9,13 +9,13 @@ class SearchResultList extends Component {
 			const header = `${repoDetails.full_name}/${result.path}`;
 			return (
 				<ListGroupItem
-					key={i} /* this doesn't get passed for some reason */
+					key={i.toString()}
 					onClick={()=>this.props.selectCB(repoDetails.full_name, result.path)}
 					style={{cursor: "pointer"}}
 				>
 					<div
 						style={{fontSize: '16px', fontWeight: '900', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}
-						class="list-group-item-heading"
+						className="list-group-item-heading"
 						title={header}
 					>{header}</div>
 					{this.highlightedMatches(result)}
@@ -26,13 +26,13 @@ class SearchResultList extends Component {
 
 	highlightedMatches(result) {
 		return result.text_matches.map((text_match, i)=>(
-			<Fragment>
+			<Fragment key={i.toString()}>
 				<span>{text_match.fragment.slice(0, text_match.matches[0].indices[0])}</span>
 				{
 					text_match.matches.map((currentMatch, currentIndex, allMatches)=> {
 						const startOfNextMatch = currentIndex + 1 == allMatches.length ? text_match.fragment.length : allMatches[currentIndex + 1].indices[0]
 						const endOfThisMatch = currentMatch.indices[1]
-						return	<span>
+						return	<span key={currentIndex.toString()}>
 								<span style={{fontWeight:'900', fontSize:'16px'}}>{currentMatch.text}</span>
 								{`${text_match.fragment.slice(
 									endOfThisMatch,
